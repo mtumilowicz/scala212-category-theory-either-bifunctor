8a11c939c85ed5cb763659c753c619e38a897bf0
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-01-09 21:58:51 +0100

    Initial commit

---
+++ src/test/scala/FunctiorialEitherTest.scala
@@ -0,0 +1,20 @@
+import org.scalatest.{FunSuite, Matchers}
+
+/**
+  * Created by mtumilowicz on 2019-01-09.
+  */
+class FunctiorialEitherTest extends FunSuite with Matchers {
+
+  implicit def toFunctorialEither[X, Y](either: Either[X, Y]) = new FunctiorialEither(either)
+
+  test("testBimap") {
+    Right(1).map(_ * 2) should be (Right(2))
+    Left[String, Integer]("a").map(_ * 2) should be (Left("a"))
+  }
+
+  test("testMap") {
+    Right[Integer, String]("a").bimap(_ * 2, _ + " mapped") should be (Right("a mapped"))
+    Left[Integer, String](1).bimap(_ * 2, _ + " mapped") should be (Left(2))
+  }
+
+}
