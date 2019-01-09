90f71b1136688fa516e27d8ad398ffcafd572971
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-01-09 22:16:21 +0100

    refactoring

--- src/main/scala/BiFunctiorialEither.scala
+++ src/main/scala/BiFunctiorialEither.scala
@@ -1,6 +1,10 @@
 /**
   * Created by mtumilowicz on 2019-01-09.
   */
-class BiFunctiorialEither[X, Y](val either : Either[X, Y]) {
-  def bimap[X1, Y1](first: X => X1, second: Y => Y1): Either[X1, Y1] = either.map(second).swap.map(first).swap
+class BiFunctiorialEither[X, Y](val either: Either[X, Y]) {
+  // either.map(second).swap.map(first).swap
+  def bimap[X1, Y1](first: X => X1, second: Y => Y1): Either[X1, Y1] = either match {
+    case Left(x) => Left(first(x))
+    case Right(y) => Right(second(y))
+  }
 }
--- src/test/scala/BiFunctiorialEitherTest.scala
+++ src/test/scala/BiFunctiorialEitherTest.scala
@@ -6,12 +6,7 @@ import org.scalatest.{FunSuite, Matchers}
 class BiFunctiorialEitherTest extends FunSuite with Matchers {
 
   implicit def toBiFunctorialEither[X, Y](either: Either[X, Y]) = new BiFunctiorialEither(either)
-
-  test("testBimap") {
-    Right(1).map(_ * 2) should be (Right(2))
-    Left[String, Integer]("a").map(_ * 2) should be (Left("a"))
-  }
-
+  
   test("testMap") {
     Right[Integer, String]("a").bimap(_ * 2, _ + " mapped") should be (Right("a mapped"))
     Left[Integer, String](1).bimap(_ * 2, _ + " mapped") should be (Left(2))
