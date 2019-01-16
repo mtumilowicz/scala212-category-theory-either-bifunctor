dd0100900456ff7f5b67f0e639158509e95a5bc7
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-01-16 19:57:26 +0100

    derived functions + tests

--- src/main/scala/BiFunctiorialEither.scala
+++ src/main/scala/BiFunctiorialEither.scala
@@ -2,9 +2,14 @@
   * Created by mtumilowicz on 2019-01-09.
   */
 class BiFunctiorialEither[X, Y](val either: Either[X, Y]) {
-  // either.map(second).swap.map(first).swap
+
   def bimap[X1, Y1](first: X => X1, second: Y => Y1): Either[X1, Y1] = either match {
     case Left(x) => Left(first(x))
     case Right(y) => Right(second(y))
   }
+
+  def first[X1](f: Function[X, X1]) = bimap(f, identity)
+
+  def second[Y1](f: Function[Y, Y1]) = bimap(identity, f)
+  
 }
--- src/test/scala/BiFunctiorialEitherTest.scala
+++ src/test/scala/BiFunctiorialEitherTest.scala
@@ -8,8 +8,20 @@ class BiFunctiorialEitherTest extends FunSuite with Matchers {
   implicit def toBiFunctorialEither[X, Y](either: Either[X, Y]) = new BiFunctiorialEither(either)
 
   test("bimap test") {
-    Right[Integer, String]("a").bimap(_ * 2, _ + " mapped") should be(Right("a mapped"))
-    Left[Integer, String](1).bimap(_ * 2, _ + " mapped") should be(Left(2))
+    Right[Int, String]("a").bimap(_ * 2, _ + " mapped") should be(Right("a mapped"))
+    Left[Int, String](1).bimap(_ * 2, _ + " mapped") should be(Left(2))
+  }
+
+  test("function first - derived from bimap") {
+    def f(i: Int) = i * 2
+    
+    Left[Int, String](1).first(f) should be(Left(2))
+  }
+
+  test("function second - derived from bimap") {
+    def f(s: String) = s + " mapped"
+    
+    Right[Int, String]("a").second(f) should be(Right("a mapped"))
   }
 
 }
